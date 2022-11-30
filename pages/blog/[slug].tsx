77c0b99b7eff790ef12e9ede37ed64ca924b2bf1
Tad Scritchfield <tadscritch@gmail.com>
2022-11-29 22:36:58 -0600

    add local studio

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { getClient, usePreviewSubscription } from "@lib/sanity";
+import { groq } from "next-sanity";
+
+const query = groq`
+*[_type == "post"] | order(_createdAt desc) {
+  ..., 
+  author->,
+  categories[]->
+}
+`;
+
+export default function Post(props) {
+  const { postdata, preview } = props;
+
+  const router = useRouter();
+
+  const { data: posts } = usePreviewSubscription(query, {
+    initialData: postdata,
+    enabled: preview || router.query.preview !== undefined,
+  });
+  return (
+    <>
+      {posts &&
+        posts.map((post, i) => (
+          <article key={i}>
+            <h3 className="text-lg"> {post.title} </h3>
+            <p className="mt-3">{post.excerpt}</p>
+          </article>
+        ))}
+    </>
+  );
+}
+
+export async function getStaticProps({ params, preview = false }: any) {
+  console.log("params", params);
+
+  const post = await getClient(preview).fetch(query);
+
+  return {
+    props: {
+      postdata: post,
+      preview,
+    },
+    revalidate: 10,
+  };
+}
