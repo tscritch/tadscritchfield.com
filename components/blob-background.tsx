8a29cfd06eb90b13e88c8d1fcb08239dcd9aa23e
Tad Scritchfield <tadscritch@gmail.com>
2022-12-28 23:30:12 -0600

    fix font, add noise to bg

--- components/blob-background.tsx
+++ components/blob-background.tsx
@@ -7,13 +7,7 @@ import styles from "../styles/blob.module.css";
 export default function BlobBackground({ children }: any) {
   return (
     <div className="h-full bg-slate-900 flex flex-col items-center justify-center relative overflow-hidden">
-      <Head>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=panchang@500,800&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
+      <NoiseBackground />
       <div className={styles.shapeBlob} />
       <div className={styles.shapeBlob2} />
       <div className={styles.shapeBlob3} />
@@ -48,3 +42,67 @@ export default function BlobBackground({ children }: any) {
     </div>
   );
 }
+
+const NoiseBackground = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      // xmlns:xlink="http://www.w3.org/1999/xlink"
+      // xmlns:svgjs="http://svgjs.dev/svgjs"
+      viewBox="0 0 100% 100%"
+      width="100%"
+      height="100%"
+      opacity="1"
+      className="absolute top-0 left-0 z-10 opacity-5"
+    >
+      <defs>
+        <filter
+          id="nnnoise-filter"
+          x="-20%"
+          y="-20%"
+          width="140%"
+          height="140%"
+          filterUnits="objectBoundingBox"
+          primitiveUnits="userSpaceOnUse"
+          color-interpolation-filters="linearRGB"
+        >
+          <feTurbulence
+            type="turbulence"
+            // baseFrequency="0.167"
+            baseFrequency="0.1"
+            numOctaves="4"
+            seed="15"
+            stitchTiles="stitch"
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            result="turbulence"
+          ></feTurbulence>
+          <feSpecularLighting
+            surfaceScale="15"
+            specularConstant="3"
+            specularExponent="20"
+            lighting-color="#fff"
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            in="turbulence"
+            result="specularLighting"
+          >
+            <feDistantLight azimuth="3" elevation="141"></feDistantLight>
+          </feSpecularLighting>
+        </filter>
+      </defs>
+      <rect width="100%" height="100%" fill="transparent"></rect>
+      <rect
+        width="100%"
+        height="100%"
+        fill="#fff"
+        filter="url(#nnnoise-filter)"
+      ></rect>
+    </svg>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,49 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
 import BlobBackground from "components/blob-background";
+import "../styles/globals.css";
+
+const panchang = localFont({
+  src: [
+    {
+      path: "./fonts/Panchang-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Panchang-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Panchang-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Panchang-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Panchang-Bold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Panchang-Extrabold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <BlobBackground>
-      <Component {...pageProps} />
-    </BlobBackground>
+    <main className={panchang.className} style={{ height: "100%" }}>
+      <BlobBackground>
+        <Component {...pageProps} />
+      </BlobBackground>
+    </main>
   );
 }
